8b14d4c034a5496c7b5e32e58f5af8e2df43e830
UberSurgeon <yepplaz@gmail.com>
2025-05-18 14:31:53 +0300

    done anecdotes

---
+++ part1/anecdotes/introdemo/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => {
+  return(
+    <>
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Array(8).fill(0))
+  const [highidx, sethighidx] = useState(0)
+  
+
+  const selectRandom = () => {setSelected(Math.floor(Math.random() * 8))}
+  const voting = () => {
+    const copy = [...vote]
+    copy[selected] += 1
+    setVote(copy)
+
+    let buff = 0
+    for(let i = 0; i < copy.length; i++){
+      if (buff < copy[i]) {
+        console.log(i)
+        buff = copy[i]
+        sethighidx(i)
+      }
+    }
+  }
+
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {vote[selected]} vote</p>
+      <Button onClick={voting} text={"vote"} />
+      <Button onClick={selectRandom} text={"next anecdote"} />
+      
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[highidx]}
+      <p>has {vote[highidx]} vote</p>
+    </div>
+  )
+}
+
+export default App
