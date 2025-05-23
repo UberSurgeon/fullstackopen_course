d800d2b7528da5b493383983ec862a811fcf556e
UberSurgeon <yepplaz@gmail.com>
2025-05-23 10:03:51 +0300

    switch phonebook to json data server

--- part2/phonebook/introdemo/src/App.jsx
+++ part2/phonebook/introdemo/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const RenderNumber = ({person}) => {
   return(
@@ -63,17 +63,18 @@ const Persons = ({showALL, persons}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState(
-    [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
-  ) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [showALL, setNewShowAll]= useState('')
+
+  useEffect(()=> {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(reponse => {
+        setPersons(reponse.data)
+      })
+  }, [])
 
   const addPb = (event) => {
     event.preventDefault()
